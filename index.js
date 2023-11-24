6134a73d65e6d334f664c1f97f548ee7b0eb2f34
Malek Darwish <malekksa505@gmail.com>
2023-11-24 13:03:58 +0300

    #

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const Product = require('./models/product')
+const Farm = require('./models/farm')
 const methodOverride = require('method-override')
 
-mongoose.connect('mongodb://127.0.0.1:27017/fruitShop')
+mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
 .then(()=> {
     console.log('Successfully Connection');
 })
@@ -21,6 +22,52 @@ app.set('view engine', 'ejs')
 
 /* Route */
 
+
+/* Farm Routes */
+
+app.get('/farms', async(req,res) => {
+    const farms = await Farm.find({});
+    res.render('farms/index', {farms})
+})
+
+app.post('/farms', async(req,res) => {
+    const farm = await new Farm(req.body);
+    await farm.save();
+    res.redirect('/farms')
+})
+
+app.get('/farms/new', (req,res) => {
+    res.render('farms/new')
+})
+
+app.get('/farms/:id', async(req,res) => {
+    const farm = await Farm.findById(req.params.id).populate('products')
+    res.render('farms/show', { farm })
+})
+
+app.get('/farms/:id/products/new', async(req,res) => {
+    const {id} = req.params;
+    const farmName = await Farm.findById(id).populate('products','name')
+    res.render('products/new', {categories, id,farmName})
+})
+
+app.post('/farms/:id/products', async(req,res) => {
+    const {id} = req.params;
+    const farm = await Farm.findById(id);
+    const product = new Product(req.body);
+    farm.products.push(product);
+    product.farm = farm;
+    await farm.save();
+    await product.save();
+    res.redirect(`/farms/${id}`);
+})
+
+app.delete('/farms/:id', async(req,res) => {
+    const farm = await Farm.findByIdAndDelete(req.params.id);
+    res.redirect('/farms')
+})
+
+/* Product Routes */
 const categories = ['fruit', 'vegetable', 'dairy']
 
 app.get('/products' , async (req,res) => {
@@ -55,7 +102,7 @@ app.post('/products', async (req,res)=> {
 
 app.get('/products/:id', async (req,res)=>{
     const {id} = req.params;
-    const findProduct = await Product.findById(id);
+    const findProduct = await Product.findById(id).populate('farm', 'name');
     res.render('products/show', {findProduct})
 })
 
